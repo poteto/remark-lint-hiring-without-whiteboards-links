5f265d0674b9eca29f9b3b3f2e55a643eebbf902
Lauren Tan <laurent@netflix.com>
2017-03-23 15:54:42 -0700

    Reduce dupe lint warnings

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ const processor = remark().use(lint).use(links);
 
 const markdown = `
 - [MyCompany](https://example.com/jobs) | San Francisco, CA; Tokyo, Japan | My description
+- [MyCompany](https://example.com/jobs) | Remote | My description
 - [MyCompany](https://example.com/jobs) | San Francisco, CA; Tokyo, Japan | | |
 - [MyCompany](https://example.com/jobs) | Tokyo | My description
 - [MyCompany](https://example.com/jobs) | Melbourne, Victoria, Australia | My description
@@ -20,17 +21,13 @@ test('remark-lint-hiring-without-whiteboards-links', (t) => {
   t.deepEqual(
     processor.processSync(markdown).messages.map(String),
     [
-      '3:1-3:80: Too many pipes, expected ` | location | description` after link',
-      '4:1-4:65: Invalid location `Tokyo`, please include city and country name',
-      '5:1-5:90: Invalid location `Melbourne, Victoria, Australia`, please only include city and country name',
-      '6:1-6:71: Invalid remote value `100% remote`, please only use `Remote`',
-      '6:1-6:71: Invalid location `100% remote`, please include city and country name',
-      '7:1-7:71: Invalid remote value `distributed`, please only use `Remote`',
-      '7:1-7:71: Invalid location `distributed`, please include city and country name',
-      '8:1-8:81: Invalid remote value `anywhere in the world`, please only use `Remote`',
-      '8:1-8:81: Invalid location `anywhere in the world`, please include city and country name',
-      '9:1-9:71: Invalid remote value `100% online`, please only use `Remote`',
-      '9:1-9:71: Invalid location `100% online`, please include city and country name'
+      '4:1-4:80: Too many pipes, expected ` | location | description` after link',
+      '5:1-5:65: Invalid location `Tokyo`, please include city and country name',
+      '6:1-6:90: Invalid location `Melbourne, Victoria, Australia`, please only include city and country name',
+      '7:1-7:71: Invalid remote value `100% remote`, please only use `Remote`',
+      '8:1-8:71: Invalid remote value `distributed`, please only use `Remote`',
+      '9:1-9:81: Invalid remote value `anywhere in the world`, please only use `Remote`',
+      '10:1-10:71: Invalid remote value `100% online`, please only use `Remote`'
     ],
     'should validate'
   );
